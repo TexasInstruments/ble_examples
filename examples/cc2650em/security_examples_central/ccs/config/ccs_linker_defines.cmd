6e689ae05a77c33364463b5bae15c92a93045d00
Tim Camise <a0221118@ti.com>
2016-06-07 17:00:27 -0700

    LPRFAPPS-355: adding CCS projects for security examples, fixing path names in IAR projects, changing debugger in CCS files

---
+++ examples/cc2650em/security_examples_central/ccs/config/ccs_linker_defines.cmd
@@ -0,0 +1,10 @@
+/*
+** Stack Frontier Generator 1.1.0 (2016-06-07 16:16:43.657000)
+**
+** WARNING - Auto-generated file. Modifications could be lost!
+*/
+
+--define=ICALL_RAM0_START=0x20004228
+--define=ICALL_STACK0_START=0xc000
+--define=ICALL_STACK0_ADDR=0xc001
+
---
+++ examples/cc2650em/security_examples_peripheral/ccs/config/ccs_linker_defines.cmd
@@ -0,0 +1,10 @@
+/*
+** Stack Frontier Generator 1.1.0 (2016-06-07 16:34:25.390000)
+**
+** WARNING - Auto-generated file. Modifications could be lost!
+*/
+
+--define=ICALL_RAM0_START=0x20004210
+--define=ICALL_STACK0_START=0xc000
+--define=ICALL_STACK0_ADDR=0xc001
+
