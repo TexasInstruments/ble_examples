2a836a8bab6349fc4c53c54940a04b19ea52b2e7
Tom Kopriva <a0273433@ti.com>
2016-06-07 17:51:16 -0700

    Updated to new version...again

--- examples/cc2650lp/hid_emu_kbd/iar/config/lib_linker.cmd
+++ examples/cc2650lp/hid_emu_kbd/iar/config/lib_linker.cmd
@@ -3,6 +3,6 @@
  *                step by the lib_search utility
  */
 
-"C:\ti\simplelink\ble_sdk_2_02_00_14\blelib\host\host_pxxx.a"
-"C:\ti\simplelink\ble_sdk_2_02_00_14\blelib\ctrl\cc2640\cc2640_ctrl_pxxx.a"
-"C:\ti\simplelink\ble_sdk_2_02_00_14\blelib\hci_tl\cc26xx\cc26xx_hci_tl_none.a"
+"C:\ti\simplelink\ble_sdk_2_02_00_25\blelib\host\host_pxxx.a"
+"C:\ti\simplelink\ble_sdk_2_02_00_25\blelib\ctrl\cc2640\cc2640_ctrl_pxxx.a"
+"C:\ti\simplelink\ble_sdk_2_02_00_25\blelib\hci_tl\cc26xx\cc26xx_hci_tl_none.a"
