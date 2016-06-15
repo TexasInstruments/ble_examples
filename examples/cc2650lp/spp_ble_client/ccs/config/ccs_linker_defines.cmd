c34802fe8b1ca3823da8ba9e6934ba7799364dbb
Zahid Haq <a0273609@ti.com>
2016-06-14 18:15:49 -0700

    upgrade to latest sdk(29) and rtos(03) before 2.2 RTM

---
+++ examples/cc2650lp/spp_ble_client/ccs/config/ccs_linker_defines.cmd
@@ -0,0 +1,10 @@
+/*
+** Stack Frontier Generator 1.1.0 (2016-03-28 14:11:07.308000)
+**
+** WARNING - Auto-generated file. Modifications could be lost!
+*/
+
+--define=ICALL_RAM0_START=0x20004368
+--define=ICALL_STACK0_START=0xe000
+--define=ICALL_STACK0_ADDR=0xe001
+
