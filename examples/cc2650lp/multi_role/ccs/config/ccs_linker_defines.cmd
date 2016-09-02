725693dab12effc042dfda7d9b32d3792a119cae
Tim Camise <a0221118@ti.com>
2016-09-02 15:11:23 -0700

    LPRFAPPS-649: creating CCS project

--- examples/cc2650lp/multi_role/ccs/config/ccs_linker_defines.cmd
+++ examples/cc2650lp/multi_role/ccs/config/ccs_linker_defines.cmd
@@ -1,10 +1,10 @@
 /*
-** Stack Frontier Generator 1.1.0 (2016-06-20 16:14:33.531000)
+** Stack Frontier Generator 1.1.0 (2016-09-02 15:06:49.547000)
 **
 ** WARNING - Auto-generated file. Modifications could be lost!
 */
 
---define=ICALL_RAM0_START=0x20004330
---define=ICALL_STACK0_START=0xc000
---define=ICALL_STACK0_ADDR=0xc001
+--define=ICALL_RAM0_START=0x200042f8
+--define=ICALL_STACK0_START=0xa000
+--define=ICALL_STACK0_ADDR=0xa001
 
