8e6cd67fdfccf118bc90b1ac0689b0d2072461a8
Thomas Kopriva <a0273433@ti.com>
2016-11-08 11:57:32 -0600

    Merge pull request #17 in SA/ble_examples from feature/simple_peripheral_observer_221 to ble_examples-develop
    Squashed commit of the following:
    
    commit 8266e4e89b41431f6d695f27eafb04fe384034ed
    Author: Haq <zhaq@ti.com>
    Date:   Mon Nov 7 11:08:46 2016 -0800
    
        added included directory based on feedback on pull request
    
        see LPRFAPPS-701 for more info
    
    commit 50dafbdaac6badda06b82762b860df40e7f723c7
    Author: Haq <zhaq@ti.com>
    Date:   Thu Nov 3 17:29:10 2016 -0700
    
        ported simple_observer_peripheral to SDK v2.2.1
    
        see LPRFAPPS-701 for more info

--- examples/cc2650lp/simple_peripheral_observer/ccs/config/ccs_linker_defines.cmd
+++ examples/cc2650lp/simple_peripheral_observer/ccs/config/ccs_linker_defines.cmd
@@ -1,10 +1,10 @@
 /*
-** Stack Frontier Generator 1.1.0 (2016-07-22 16:44:48.748000)
+** Stack Frontier Generator 1.1.0 (2016-11-03 17:22:26.852000)
 **
 ** WARNING - Auto-generated file. Modifications could be lost!
 */
 
---define=ICALL_RAM0_START=0x20004320
+--define=ICALL_RAM0_START=0x20004300
 --define=ICALL_STACK0_START=0xd000
 --define=ICALL_STACK0_ADDR=0xd001
 
