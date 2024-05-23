c9306d70092fb0723058045353d3795b5153ef50
Ivan De Padua <i-depadua@ti.com>
2024-05-23 17:09:34 +0200

    removed trashed files

--- examples/rtos/LP_EM_CC1354P10_1/ble32xx_connection_v1_central/simple_central_LP_EM_CC1354P10_1_tirtos7_ble32x_v1/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-lti_utils_build_linker.cmd.genlibs -Wl,-l"ti/devices/cc13x4_cc26x4/driverlib/bin/ticlang/driverlib.lib" -Wl,-llibc.a
-
